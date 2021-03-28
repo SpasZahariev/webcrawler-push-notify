a094d801ced484cd8041863356688879f6d202c0
Spas_Z <spaszahariev54@gmail.com>
2021-03-28 18:16:40 +0100

    initial commit

---
+++ scrape.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+from datetime import datetime
+from twilio.rest import Client
+from decouple import config
+import time
+import sched
+
+
+# twilio config
+account_sid = config("ACCOUNT_SID")
+auth_token = config("AUTH_TOKEN")
+client = Client(account_sid, auth_token)
+
+my_url = "https://app.beefy.finance/"
+browser = webdriver.Chrome(
+    executable_path=("J:\dev-tools\ChromeDriver\chromedriver.exe")
+)
+browser.get(my_url)
+
+# Creating an instance of the
+# scheduler class
+scheduler = sched.scheduler(time.time, time.sleep)
+
+
+def send_sms():
+    content = "Alert from PY web crawler at: " + str(datetime.now())
+    message = client.messages.create(
+        body=content, from_=config("TWILIO_PHONE"), to=config("MY_PHONE")
+    )
+    print(message.sid)
+
+
+# refresh webpage and notify me if anything has changed in the last 5 mins
+def check_tokens(cached_tokens):
+
+    # refreshing
+    browser.refresh()
+
+    # "p.MuiTypography-root.MuiTypography-body2.MuiTypography-gutterBottom"
+    token_containers = browser.find_elements_by_css_selector(
+        "div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-3"
+    )
+
+    token_names = map(
+        lambda container: container.find_element_by_css_selector(
+            "p.MuiTypography-root.MuiTypography-body2.MuiTypography-gutterBottom"
+        ).text,
+        token_containers,
+    )
+
+    retrieved_tokens = set(token_names)
+
+    print("Tick at: ", datetime.now())
+    if cached_tokens != retrieved_tokens:
+        print("------ Alert! -------- ")
+        print("new tokens: ", retrieved_tokens)
+        cached_tokens = retrieved_tokens
+        send_sms()
+
+    scheduler.enter(300, 1, check_tokens, (cached_tokens,))
+
+
+cached_tokens = set()
+
+# enter(delay in seconds, task Priority, function to be called, tupleOfParameters to be provided to function)
+scheduler.enter(5, 1, check_tokens, (cached_tokens,))
+scheduler.run()
